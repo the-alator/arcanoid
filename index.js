d8faa2a562b6ff4e0f93134373c8c55677175c5b
the-alator <oleg.r646@gmail.com>
2018-04-03 23:51:37 +0300

    everything is good! Exept of score... until next day

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var canvas = document.getElementById("mainCanv");
 var draw = canvas.getContext("2d");
 
-var ballX = canvas.width/2, ballY = canvas.height - canvas.height/4;
-var dx = 1, dy = -1;
+var gameStatus;
+var gameStatuses = {RUNNING: 1, STOPPED: 2 };
+
+var ballX, ballY ;
+var dx, dy;
 var radius = 10;
+var ballState;
+var ballStates = {STATIC: 1, MOVING: 2, INVISIBLE:3};
 
 var brickColumns=10;
 var brickRows=6;
@@ -14,26 +19,18 @@ var brickVGap;
 var brickHGap;
 
 var bricks  = [];
+var brickCount = brickColumns * brickRows;
 
 var platformX, platformY;
 var platformWidth, platformHeight;
+var basicPlatformHeight = 0;
+var platformHeightDecr;
+var size_life = 3;
+
 ///////////////////////////
 
-// var arr = [];
-// arr[0] = 1;
-// arr[1] = null;
-// arr[2] = 3;
-// arr[3] = undefined;
-// arr[4] = 5;
-// arr[5] = undefined;
-// arr[100] = undefined;
-//
-// for(var val of arr)
-//     console.log(val);
-
-
 init();
-setInterval(loop, 3);
+var interval = setInterval(loop, 1);
 
 /////////////////
 
@@ -41,15 +38,16 @@ function loop(){
     var nextBallX = ballX + dx;
     var nextBallY = ballY + dy;
     brickCollision(nextBallX,nextBallY);
-    //check next step
-    if(ballX + dx < 0 || ballX + dx > canvas.width)
-        dx = -dx;
-    if(ballY + dy < 0 || ballY + dy > canvas.height)
-        dy = -dy;
+    borderCollision(nextBallX,nextBallY);
+    platformCollision(nextBallX,nextBallY);
+
     //go to next step
-    ballX += dx;
-    ballY += dy;
-    repaint();
+    if(ballState == ballStates.MOVING) {
+        ballX += dx;
+        ballY += dy;
+    }
+    if(gameStatus === gameStatuses.RUNNING)
+        repaint();
 }
 function brickCollision(nextBallX, nextBallY){
     var ballArrX = (ballX / (brickWidth + brickHGap)) | 0;
@@ -57,14 +55,103 @@ function brickCollision(nextBallX, nextBallY){
     if(ballArrY > brickRows)
         return;
     var index;
+    index = (ballArrY - 1) * brickColumns + ballArrX - 1;
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x + brickWidth, bricks[index].y + brickHeight, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+
     index = (ballArrY - 1) * brickColumns + ballArrX;
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x, bricks[index].y + brickHeight, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x + brickWidth, bricks[index].y + brickHeight, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
     if(bricks[index] != undefined && brickHorizontalSideCollision(bricks[index].x, bricks[index].x + brickWidth, bricks[index].y + brickHeight,nextBallX,nextBallY)) {
         destroyBrick(index);
         return;
     }
 
+    index = (ballArrY - 1) * brickColumns + ballArrX + 1;
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x, bricks[index].y + brickHeight, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+
+    index = ballArrY * brickColumns + ballArrX + 1;
+    if(bricks[index] != undefined && brickVerticalSideCollision(bricks[index].y, bricks[index].y + brickHeight, bricks[index].x + brickWidth,nextBallX,nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x, bricks[index].y, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x, bricks[index].y + brickHeight, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+
+    index = (ballArrY + 1) * brickColumns + ballArrX + 1;
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x, bricks[index].y, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+
+    index = (ballArrY + 1) * brickColumns + ballArrX;
+    if(bricks[index] != undefined && brickHorizontalSideCollision(bricks[index].x, bricks[index].x + brickWidth, bricks[index].y,nextBallX,nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x, bricks[index].y, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x + brickWidth, bricks[index].y, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+
+    index = (ballArrY + 1) * brickColumns + ballArrX - 1;
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x + brickWidth, bricks[index].y, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+
+    index = ballArrY * brickColumns + ballArrX - 1;
+    if(bricks[index] != undefined && brickVerticalSideCollision(bricks[index].y, bricks[index].y + brickHeight, bricks[index].x,nextBallX,nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x + brickWidth, bricks[index].y, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
+    if(bricks[index] != undefined && brickCornerCollision(bricks[index].x + brickWidth, bricks[index].y + brickHeight, nextBallX, nextBallY)) {
+        destroyBrick(index);
+        return;
+    }
 }
 function brickCornerCollision(brickX, brickY, ballX, ballY){
+    var math = Math.sqrt(Math.pow(brickX - ballX, 2) + Math.pow(brickY - ballY, 2));
+    if(interval == 0)
+        console.log(math + " ballX " + ballX + " ballY " + ballY + " brickX " + brickX + " brickY " + brickY);
+    if(math < radius){
+        if(Math.abs(brickY - ballY) == Math.abs(brickX - ballX)){
+            dx = -dx;
+            dy = -dy;
+        }
+        else if(Math.abs(brickY - ballY) > Math.abs(brickX - ballX))
+            dy = -dy;
+        else
+            dx = -dx;
+
+        console.log("amazing!");
+        return true;
+    }
 
 }
 function brickHorizontalSideCollision(brickStartX, brickEndX, brickY, ballX, ballY){
@@ -74,22 +161,199 @@ function brickHorizontalSideCollision(brickStartX, brickEndX, brickY, ballX, bal
     }
 }
 function brickVerticalSideCollision(brickStartY, brickEndY, brickX, ballX, ballY){
-    if(ballY >= brickStartY && ballY <= brickEndY && Math.abs(ballX - brickX) < radius)
+    if(ballY >= brickStartY && ballY <= brickEndY && Math.abs(ballX - brickX) < radius){
         dx = -dx;
+        return true;
+    }
 }
-function plaformCollision(){
+function platformCollision(nextBallX, nextBallY){
+    if(brickHorizontalSideCollision(platformX, platformX + platformWidth, platformY, nextBallX, nextBallY)) {
+        console.log("Collision at " + (ballX - platformX) + " with platformX " + platformX + " ballX " + ballX + " platformWidth " + platformWidth);
+        dx = -Math.cos(((ballX - platformX) / platformWidth) * Math.PI);
+        dy = -(2 - Math.abs(dx));
+        console.log("Calc angle " + (((ballX - platformX) / platformWidth) * 180));
+        console.log(dx);
+    }
+}
+function borderCollision(ballX, ballY){
+    if(ballX - radius <= 0 || ballX + radius >= canvas.width)
+        dx = -dx;
+    if(ballY - radius <= 0)
+        dy = -dy;
+    if(ballY + radius >= canvas.height) {
+        size_life--;
+        resizePlatform();
+        if(size_life < 0)
+            gameOver();
+        createBall();
 
-}
-function borderCollision(){
+    }
 
 }
 function destroyBrick(index){
     bricks[index] = null;
+    console.log("splitter");
+    brickCount--;
+    if(brickCount <= 0)
+        gameWin();
 }
+
+var fontSize = canvas.height / 20;
+var mainTextY = canvas.height / 6;
+function endGame(){
+    gameStatus = gameStatuses.STOPPED;
+    document.removeEventListener("mousemove",mouseMoved);
+    document.removeEventListener("keydown",keyPressed);
+    clearInterval(interval);
+    draw.globalAlpha = 0.2;
+    destroyBall();
+    repaint();
+
+    draw.globalAlpha = 1;
+    draw.font = fontSize + "px Comic Sans MS";
+    var restartText = "Press LMB to restart game";
+    draw.fillStyle = "red";
+    draw.fillText(restartText,(canvas.width - draw.measureText(restartText).width) / 2, canvas.height - fontSize - canvas.height / 15);
+}
+function gameOver(){
+    endGame();
+    var endText = "You lost!";
+    draw.fillText(endText, (canvas.width - draw.measureText(endText).width) / 2,mainTextY);
+}
+function gameWin(){
+    endGame();
+    var endText = "You won! Congratulations!";
+    draw.fillStyle = "green";
+    draw.fillText(endText, (canvas.width - draw.measureText(endText).width) / 2,mainTextY);
+}
+
+
+function mouseDown(event){
+    if(document.documentElement.clientWidth/8 < event.x && document.documentElement.clientWidth/8 + canvas.width > event.x &&
+        document.documentElement.clientHeight/12 < event.y && document.documentElement.clientHeight/12 + canvas.height > event.y){
+        if(gameStatus === gameStatuses.RUNNING){
+            if(ballState === ballStates.STATIC) {
+                ballState = ballStates.MOVING;
+            }
+        }else{
+            document.removeEventListener("mousedown",mouseDown);
+            document.location.reload();
+        }
+
+    }
+
+
+}
+function keyPressed(event){
+    if(event.key === 'o'){
+        gameOver();
+    }
+    if(event.key === 'w'){
+        gameWin();
+    }
+    if(event.key === 'g'){
+        if(interval != 0){
+            clearInterval(interval);
+            interval = 0;
+        }
+        else
+            interval = setInterval(loop,1);
+    }
+    if(interval === 0){
+        if(event.key == 1){
+            dx = -1;
+            dy = 1;
+        }
+        else if(event.key == 7){
+            dx = -1;
+            dy = -1;
+        }
+        else if(event.key == 9){
+            dx = 1;
+            dy = -1;
+        }
+        else if(event.key == 3){
+            dx = 1;
+            dy = 1;
+        }
+        else if(event.key == 5){
+            loop();
+            console.log("here");
+        }
+    }
+
+}
+function mouseMoved(event){
+    var tempPlatformX = event.x - platformWidth / 2 - document.documentElement.clientWidth/8;
+    if(tempPlatformX <= 0)
+        setPlatformX(0);
+    else
+    if(tempPlatformX >= canvas.width - platformWidth)
+        setPlatformX(canvas.width - platformWidth);
+    else
+        setPlatformX(tempPlatformX);
+    if(interval === 0)
+        repaint();
+    function setPlatformX(x){
+        platformX = x;
+        if(ballState === ballStates.STATIC)
+            ballX = platformX + (platformWidth / 2);
+    }
+}
+
+function init(){
+    document.addEventListener("mousemove",mouseMoved);
+    document.addEventListener("keydown",keyPressed);
+    document.addEventListener("mousedown",mouseDown);
+    createBricks();
+    createPlatform();
+    createBall();
+    gameStatus = gameStatuses.RUNNING;
+}
+function createBricks(){
+    var currentBrick;
+    brickHGap = (canvas.width / 10) /  (brickColumns + 1);
+    brickWidth = (canvas.width - canvas.width / 10) / brickColumns;
+    brickHeight = brickWidth / 5;
+    brickVGap = brickHeight / 2;
+    for(var a = 0; a < brickRows; a++){
+        for(var b = 0; b < brickColumns; b++){
+            currentBrick = {};
+            currentBrick.x = brickHGap + brickHGap*b + brickWidth*b;
+            currentBrick.y = brickVGap + brickVGap*a + brickHeight*a;
+            currentBrick.num = a * brickColumns + b + 1;
+            bricks.push(currentBrick);
+        }
+    }
+}
+function createPlatform(){
+    platformWidth = canvas.width / 7;
+    platformHeight = platformWidth / 10;
+    basicPlatformHeight = platformHeight;
+    platformHeightDecr = platformHeight / (size_life + 1);
+    platformX = (canvas.width - platformWidth) / 2;
+    platformY = canvas.height - basicPlatformHeight * 2;
+}
+function resizePlatform(){
+    platformHeight -= platformHeightDecr;
+    platformY = canvas.height - basicPlatformHeight - platformHeight;
+}
+function createBall(){
+    ballX =  platformX + (platformWidth / 2);
+    ballY = platformY - basicPlatformHeight;
+    dx = 1;
+    dy = -1;
+    ballState = ballStates.STATIC;
+}
+function destroyBall(){
+    ballState = ballStates.INVISIBLE;
+}
+
 function repaint(){
     draw.clearRect(0,0,canvas.width, canvas.height);
-
-    drawBall();
+    if(ballState !== ballStates.INVISIBLE) {
+        drawBall();
+    }
     drawBricks();
     drawPlatform();
 }
@@ -109,6 +373,9 @@ function drawBricks(){
         draw.rect(bricks[a].x,bricks[a].y,brickWidth,brickHeight);
         draw.fill();
         draw.closePath();
+        draw.font = (brickHeight - brickHeight / 3) + "px Georgia";
+        draw.fillStyle = "yellow";
+        draw.fillText(bricks[a].num,bricks[a].x + (brickWidth - draw.measureText(bricks[a].num).width)/2, bricks[a].y + brickHeight / 3 * 2 );
     }
 }
 function drawPlatform(){
@@ -117,42 +384,4 @@ function drawPlatform(){
     draw.rect(platformX, platformY, platformWidth, platformHeight);
     draw.fill();
     draw.closePath();
-}
-
-function init(){
-    document.addEventListener("mousemove",mouseMoved);
-    createBricks();
-    createPlatform();
-}
-
-function mouseMoved(event){
-    var tempPlatformX = event.x - platformWidth / 2 - document.documentElement.clientWidth/8;
-    if(tempPlatformX <= 0)
-        platformX = 0;
-    else
-    if(tempPlatformX >= canvas.width - platformWidth)
-        platformX = canvas.width - platformWidth;
-    else
-        platformX = tempPlatformX;
-}
-function createBricks(){
-    var currentBrick;
-    brickHGap = (canvas.width / 10) /  (brickColumns + 1);
-    brickWidth = (canvas.width - canvas.width / 10) / brickColumns;
-    brickHeight = brickWidth / 5;
-    brickVGap = brickHeight / 2;
-    for(var a = 0; a < brickRows; a++){
-        for(var b = 0; b < brickColumns; b++){
-            currentBrick = {};
-            currentBrick.x = brickHGap + brickHGap*b + brickWidth*b;
-            currentBrick.y = brickVGap + brickVGap*a + brickHeight*a;
-            bricks.push(currentBrick);
-        }
-    }
-}
-function createPlatform(){
-    platformWidth = canvas.width / 7;
-    platformHeight = platformWidth / 10;
-    platformX = (canvas.width - platformWidth) / 2;
-    platformY = canvas.height - platformHeight * 2;
 }
